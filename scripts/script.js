34ec29dfef5641a8d24ecdbc9a054c02afa87ef3
Jamie Wilson <howemanning@gmail.com>
2022-09-20 00:56:19 +1200

    Create js file, link from header, moved scripts to external.

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+(function () {
+  window.onload = function () {
+    var frameName = new ds07o6pcmkorn({
+      openElementId: "#shielded-logo",
+      modalID: "modal",
+    });
+    frameName.init();
+  };
+})();
