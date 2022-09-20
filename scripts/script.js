996ea7680371ba6e62fd5ff2df3186af41d8a675
Jamie Wilson <howemanning@gmail.com>
2022-09-20 16:55:07 +1200

    Add js func onclick for nav.

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,28 @@
     frameName.init();
   };
 })();
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+// function js__nav() {
+//   document.getElementById("nav__dropdown--menu").classList.toggle("show");
+// }
+
+function js__nav() {
+  var element = document.getElementById("nav__dropdown--menu");
+  element.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("nav__dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
