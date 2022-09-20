7f623a9f8a363f72ec90e4bdaaa80ab07ca721da
Jamie Wilson <howemanning@gmail.com>
2022-09-20 20:21:43 +1200

    Fill out dropdown menu content. Add functions for each heading.

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,49 @@ toggle between hiding and showing the dropdown content */
 //   document.getElementById("nav__dropdown--menu").classList.toggle("show");
 // }
 
-function js__nav() {
-  var element = document.getElementById("nav__dropdown--menu");
-  element.classList.toggle("show");
+function js__nav__services() {
+  var services = document.getElementById("nav__dropdown--menu");
+  services.classList.toggle("show");
+}
+
+function js__nav__consents() {
+  var consents = document.getElementById("nav__dropdown--consents");
+  consents.classList.toggle("show");
+}
+
+function js__nav__transport() {
+  var transport = document.getElementById("nav__dropdown--transport");
+  transport.classList.toggle("show");
+}
+
+function js__nav__environment() {
+  var environment = document.getElementById("nav__dropdown--environment");
+  environment.classList.toggle("show");
+}
+
+function js__nav__rec() {
+  var rec = document.getElementById("nav__dropdown--rec");
+  rec.classList.toggle("show");
+}
+
+function js__nav__parks() {
+  var parks = document.getElementById("nav__dropdown--parks");
+  parks.classList.toggle("show");
+}
+
+function js__nav__culture() {
+  var culture = document.getElementById("nav__dropdown--culture");
+  culture.classList.toggle("show");
+}
+
+function js__nav__news() {
+  var news = document.getElementById("nav__dropdown--news");
+  news.classList.toggle("show");
+}
+
+function js__nav__council() {
+  var council = document.getElementById("nav__dropdown--council");
+  council.classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
